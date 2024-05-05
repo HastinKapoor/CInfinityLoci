a7405ec5744d399a1c14fc5351885ff9709e979a
Bashir Abdel-Fattah <btmaf@uw.edu>
2024-05-05 15:43:37 -0700

    .

--- CInfinityLoci/CInfinityLoci.lean
+++ CInfinityLoci/CInfinityLoci.lean
@@ -9,36 +9,53 @@ notation "ℝ^"n => EuclideanSpace ℝ (Fin n)
 notation "C^∞(ℝ^"n", ℝ^"m")" => {f: (ℝ^n) → (ℝ^m) // ContDiff ℝ ⊤ f }
 notation "C^∞(ℝ^"n")" => C^∞(ℝ^n, ℝ^1)
 
+notation A"^"n => Fin n → A
 
-#check C^∞(ℝ^n, ℝ^m)
+-- variable (f : C^∞(ℝ^n, ℝ^m))
+-- #check f.1
+-- #check f.2
 
-variable (f : C^∞(ℝ^n, ℝ^m))
-#check f.1
-#check f.2
+-- variable (A : Type*)
+-- #check A^n
 
 -- How does one write an element with type EuclideanSpace ℝ (Fin n)?
--- How does one tell lean that the composition defines a map C^∞(ℝ^m, ℝ^k) × C^∞(ℝ^n, ℝ^m) → C^∞(ℝ^n, ℝ^k)?
 -- How does one show that the coordinate projections ℝ^n → ℝ are elements of C^∞(ℝ^n, ℝ^1)?
 -- How does one access the unique element of EuclideanSpace ℝ (Fin 0), or of the type (Fin 0) → α more generally?
 
 -- Outline:
 
 def π {n : ℕ} (i : Fin n) : C^∞(ℝ^n) := by
-  let f : (ℝ^n) → (ℝ^1) := sorry
+  let f : (ℝ^n) → (ℝ^1) := (fun x ↦ (fun _ ↦ x i))
   have h : ContDiff ℝ ⊤ f := by sorry
   exact ⟨f, h⟩
 
--- Define structure of a C^∞-Ring α (a C^∞-Ring taking values in the type A)
+def comp {n m k: ℕ} (g : C^∞(ℝ^m, ℝ^k)) (f : C^∞(ℝ^n, ℝ^m)) : C^∞(ℝ^n, ℝ^k) := by
+  let gf : (ℝ^n) → (ℝ^k) := g.1 ∘ f.1
+  have h : ContDiff ℝ ⊤ gf := by sorry
+  exact ⟨gf, h⟩
+
+infixr:75 " ⋄ " => comp
+-- Defines composition as a map C^∞(ℝ^m, ℝ^k) × C^∞(ℝ^n, ℝ^m) → C^∞(ℝ^n, ℝ^k)
+
+
+-- Defines structure of a C^∞-Ring α (a C^∞-Ring taking values in the type A)
 class CinftyRing (A: Type*) where
-  intrprt : ∀ {n m : ℕ} (f : C^∞(ℝ^n, ℝ^m)), (Fin n → A) → (Fin m → A)
---   fnctr : ∀ {n m k: ℕ} (f : C^∞(ℝ^n, ℝ^m)) (g : C^∞(ℝ^m, ℝ^k)), ... (something saying intrprt (f ∘ g) = (intrprt f) ∘ (intrprt g))
-  proj : ∀ {n : ℕ} (i : Fin n), intrprt (π i) = fun (a : Fin n → A) ↦ (fun (_ : Fin 1) ↦ a i)
+  intrprt : ∀ {n m : ℕ} (f : C^∞(ℝ^n, ℝ^m)), (A^n) → (A^m)
+  fnctr : ∀ {n m k: ℕ} (f : C^∞(ℝ^n, ℝ^m)) (g : C^∞(ℝ^m, ℝ^k)), intrprt (g ⋄ f) = (intrprt g) ∘ (intrprt f)
+  proj : ∀ {n : ℕ} (i : Fin n), intrprt (π i) = fun a ↦ (fun _ ↦ a i)
 
 -- define a type/structure/attribute Hom A B of C^∞-Ring homomorphisms (A: C^∞-Ring α) to (B: C^∞-Ring β)
 @[ext]
 structure CinftyRingHom (A B : Type*) [CinftyRing A] [CinftyRing B] where
   toFun : A → B
-  compat : ∀ {n m : ℕ} (f : C^∞(ℝ^n, ℝ^m)) (a : Fin n → A), (fun (i : Fin m) ↦ (toFun (CinftyRing.intrprt f a i))) = CinftyRing.intrprt f (fun (i : Fin n) ↦ toFun (a i))
+  compat : ∀ {n m : ℕ} (f : C^∞(ℝ^n, ℝ^m)) (a : A^n), (fun i ↦ (toFun (CinftyRing.intrprt f a i))) = CinftyRing.intrprt f (fun i ↦ toFun (a i))
+
+instance [CinftyRing A] [CinftyRing B] : CoeFun (CinftyRingHom A B) (fun _ ↦ A → B) where
+  coe := CinftyRingHom.toFun
+
+attribute [coe] CinftyRingHom.toFun
+
+-- define coercion to ℝ-algebra homomorphism?
 
 -- theorem saying that every C^∞-Ring is an instance of a commutative ℝ-algebra
 instance {A: Type*} [CinftyRing A] : --ℝ-algebra A :=
